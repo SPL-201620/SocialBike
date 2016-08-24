6a78bbbb8e0a2c33e0bd50978a521eb91d16090b
jfsanchezaguilar <jfsanchezaguilar@gmail.com>
2016-08-23 20:38:53 -0500

    Initial Commit for BE

---
+++ BE/src/main/java/com/socialbike/SocialBikeApplication.java
@@ -0,0 +1,12 @@
+package com.socialbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBikeApplication.class, args);
+	}
+}
---
+++ BE/src/main/java/com/socialbike/entity/User.java
@@ -0,0 +1,33 @@
+package com.socialbike.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by SAN on 23/08/2016.
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String firebaseId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirebaseId() {
+        return firebaseId;
+    }
+
+    public void setFirebaseId(String firebaseId) {
+        this.firebaseId = firebaseId;
+    }
+}
