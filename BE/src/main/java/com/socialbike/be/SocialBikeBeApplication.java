cdb9571febdf68a8d03703c708ff6c613f357009
jfsanchezaguilar <jfsanchezaguilar@gmail.com>
2016-09-11 04:24:28 -0500

    Clean Project Update

---
+++ BE/src/main/java/com/socialbike/be/SocialBikeBeApplication.java
@@ -0,0 +1,12 @@
+package com.socialbike.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBikeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBikeBeApplication.class, args);
+	}
+}
