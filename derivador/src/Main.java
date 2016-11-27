ada71100f2d2849563bca2ed37b9f875c7f77d67
VictorinoMoya <asuevis@hotmail.com>
2016-11-27 09:36:41 -0500

    Agregar las otras configuraciones

---
+++ derivador/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        System.out.println("Press return to continue.");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        in.read();
+    }
+    
+    public void ReadConfiguration(String path){
+    	
+    }
+}
